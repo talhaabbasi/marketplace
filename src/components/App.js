a2d749d13a3c3caac4893150eb07410ab5a358ea
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-06-24 15:21:56 +0500

    Initial Frontend Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import logo from "../logo.png";
+import "./App.css";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +38,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Talha Abbasi's Marketplace
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -37,7 +62,10 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  LEARN BLOCKCHAIN{" "}
+                  <u>
+                    <b>NOW! </b>
+                  </u>
                 </a>
               </div>
             </main>
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -102,22 +102,22 @@ contract("Marketplace", ([deployer, seller, buyer]) => {
       assert.equal(newSellerBalance.toString(), expectedBalance.toString());
 
       // Buy product that does not exist
-      // await marketplace.purchaseProduct(99, {
-      //   from: buyer,
-      //   value: web3.utils.toWei("1", "Ether"),
-      // }).should.be.rejected;
-      // await marketplace.purchaseProduct(productCount, {
-      //   from: buyer,
-      //   value: web3.utils.toWei("0.5", "Ether"),
-      // }).should.be.rejected;
-      // await marketplace.purchaseProduct(productCount, {
-      //   from: deployer,
-      //   value: web3.utils.toWei("1", "Ether"),
-      // }).should.be.rejected;
-      // await marketplace.purchaseProduct(productCount, {
-      //   from: buyer,
-      //   value: web3.utils.toWei("1", "Ether"),
-      // }).should.be.rejected;
+      await marketplace.purchaseProduct(99, {
+        from: buyer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, {
+        from: buyer,
+        value: web3.utils.toWei("0.5", "Ether"),
+      }).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, {
+        from: deployer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, {
+        from: buyer,
+        value: web3.utils.toWei("1", "Ether"),
+      }).should.be.rejected;
     });
   });
 });

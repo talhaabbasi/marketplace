6d3f9f1002f6645f804b6050aa41a45fa38df5b2
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-06-27 19:07:53 +0500

    Code Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,7 @@ class App extends Component {
         Marketplace.abi,
         networkData.address
       );
+      this.setState({ marketplace: marketplace, loading: false });
     } else {
       window.alert("Marketplace contract not deployed to detected network");
     }
@@ -57,30 +58,9 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN{" "}
-                  <u>
-                    <b>NOW! </b>
-                  </u>
-                </a>
+            <main role="main" className="col-lg-12 d-flex">
+              <div id="content">
+                <h1>Add Product</h1>
               </div>
             </main>
           </div>
